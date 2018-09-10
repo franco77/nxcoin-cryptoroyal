ca165bf98b5cf0d1f86d4b1e5dd60165e9d08907
mirza <sugamirza2@gmail.com>
2018-09-10 23:21:18 +0700

    fucking bug

--- application/controllers/roi/Run.php
+++ application/controllers/roi/Run.php
@@ -27,6 +27,7 @@ class Run extends CI_Controller {
 		$this->db->join('tb_package', 'package_id = stc_package', 'left');
 		$this->db->where('stc_date_end >= ', sekarang());
 		$ge = $this->db->get('tb_stacking');
+		die(json_encode($ge->result()));
 		if ($ge->num_rows() > 0){ 
 			foreach ($ge->result() as $get_stc) { 
 				$userdata = userdata(array('id' => $get_stc->stc_userid));
