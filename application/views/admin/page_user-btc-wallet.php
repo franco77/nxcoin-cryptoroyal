142956e0dfa46bb360329f03c3958900376db710
mirza <sugamirza2@gmail.com>
2018-09-10 16:14:01 +0700

    added datatables

--- application/views/admin/page_user-btc-wallet.php
+++ application/views/admin/page_user-btc-wallet.php
@@ -1,35 +1,43 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 
-<table class="table table-hover">
+<table class="table table-hover" id="btc_wallet_list">
     <thead>
         <tr>
-            <th></th>
+            <th>Label / Username</th>
+            <th>Address</th>
+            <th>Balance</th>
+            <th>Total Received</th>
         </tr>
     </thead>
-    <tbody>
-        <tr>
-            <td></td>
-        </tr>
-    </tbody>
+    <tbody></tbody>
 </table>
-
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script>
 
 $(document).ready(function() {
 
     var url = env.site_url+'wallet/listing';
-
-    $.ajax({
-        url: url,
-        method: 'GET',
-        success: function(res) {
-            console.log(res)
-        },
-        error: function(err) {
-            console.log(err)
-        }
-    }).done(function(res) {
-        console.log(res);
+    $("#btc_wallet_list").DataTable({
+        "ajax": url,
+        "columns": [
+            {"data": 'label'},
+            {"data": 'address'},
+            {"data": 'balance'},
+            {"data": 'total_received'}
+        ]
     });
+    // $.ajax({
+    //     url: url,
+    //     method: 'GET',
+    //     success: function(res) {
+    //         console.log(res)
+    //     },
+    //     error: function(err) {
+    //         console.log(err)
+    //     }
+    // }).done(function(res) {
+    //     console.log(res);
+    // });
 
 
 });
