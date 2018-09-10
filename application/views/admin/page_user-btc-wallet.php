51a96e24e268d6c69550ed505498b7bf3834938e
mirza <sugamirza2@gmail.com>
2018-09-10 17:11:55 +0700

    added refresh user btc wallet

--- application/views/admin/page_user-btc-wallet.php
+++ application/views/admin/page_user-btc-wallet.php
@@ -1,23 +1,34 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 
-<table class="table table-hover" id="btc_wallet_list">
-    <thead>
-        <tr>
-            <th>Label / Username</th>
-            <th>Address</th>
-            <th>Balance</th>
-            <th>Total Received</th>
-        </tr>
-    </thead>
-    <tbody></tbody>
-</table>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                
+                <button id="btn_refresh_wallet" type="button" class="btn btn-default">Refresh</button>
+                <table class="table table-hover" id="btc_wallet_list">
+                    <thead>
+                        <tr>
+                            <th>Label / Username</th>
+                            <th>Address</th>
+                            <th>Balance</th>
+                            <th>Total Received</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script>
 
 $(document).ready(function() {
 
     var url = env.site_url+'wallet/listing';
-    $("#btc_wallet_list").DataTable({
+    var btc_wallet_list = $("#btc_wallet_list").DataTable({
         "ajax": url,
         "columns": [
             {"data": 'label'},
@@ -26,6 +37,10 @@ $(document).ready(function() {
             {"data": 'total_received'}
         ]
     });
+    $("#btn_refresh_wallet").on('click', function() {
+        btc_wallet_list.ajax.reload();
+    })
+
     // $.ajax({
     //     url: url,
     //     method: 'GET',
