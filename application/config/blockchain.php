1255266747fc7d95bb4718294705864fb8b13808
mirza <sugamirza2@gmail.com>
2018-09-10 13:33:28 +0700

    chang guid blockchain

--- application/config/blockchain.php
+++ application/config/blockchain.php
@@ -1,8 +1,15 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['guid']				= '3f0247fd-17bf-4805-a8f7-f179649098bc';
-$config['main_password'] 	= 'IDprogrammer123';
-$config['second_password']	= null;
+// $config['guid']				= '3f0247fd-17bf-4805-a8f7-f179649098bc';
+// $config['main_password'] 	= 'IDprogrammer123';
+// $config['second_password']	= null;
+// $config['api_code']			= "0d39755a-7844-4f56-9c3d-83594417b912";
+// $config['base_url']			= 'http://127.0.0.1/';
+// $config['port']				= '3000';
+
+$config['guid']				= '431a22e0-e708-40de-b7cd-fb67d8d4290c';
+$config['main_password'] 	= 'Bismillah123';
+$config['second_password']	= '252525';
 $config['api_code']			= "0d39755a-7844-4f56-9c3d-83594417b912";
 $config['base_url']			= 'http://127.0.0.1/';
 $config['port']				= '3000';
