6f74f0f8abd30546bf43cf0c7c2c057a95f588c6
mirza <sugamirza2@gmail.com>
2018-09-26 17:33:22 +0700

    fixed send email

--- application/controllers/Command/Bonus.php
+++ application/controllers/Command/Bonus.php
@@ -16,14 +16,25 @@ class Bonus extends CI_Controller {
 
         $this->in_debug_mode = $mode;
         echo "DEBUG :$mode\n";
-        
+
         switch ($type) {
             case 'leadership':
                 $this->leadership();
                 break;
         }
         
-        $this->mailermodel->send('sugamirza2@gmail.com','Cryptoroyal - Bonus Command','its running dude!');
+        $this->mailermodel->send('sugamirza2@gmail.com','Cryptoroyal - Bonus Command','<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+        <h1>its running dude!</h1>    
+        </body>
+        </html>');
 
 
     }
